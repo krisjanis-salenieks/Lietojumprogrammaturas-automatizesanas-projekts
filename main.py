e1903b702b6500032e5e3af426cdbaa710492152
BuildTools <unconfigured@null.spigotmc.org>
2024-01-10 13:35:07 +0200

    basic tests google meklēšanai caur pitonu

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-##kods
+from googlesearch import search
+
+websites=[]
+searchQuery = input("Lūdzu ievadiet meklējamo frāzi: ")
+
+for j in search(searchQuery, tld="co.in", num=10, stop=100, pause=2):
+    websites.append(j)
+
+print(websites)
+print(len(websites))
