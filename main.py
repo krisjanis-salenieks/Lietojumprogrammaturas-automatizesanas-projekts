17d0dcb095c7d2c2921bdd33d6eda7fa75a5d0ef
BuildTools <unconfigured@null.spigotmc.org>
2024-01-13 13:11:32 +0200

    es nezinu ko talak pievienot

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 from googlesearch import search
+import random
+from selenium import webdriver
+
 
 websites=[]
-searchQuery = input("Lūdzu ievadiet meklējamo frāzi: ")
+searchQuery=input("Lūdzu ievadiet meklējamo frāzi: ")
 
 for j in search(searchQuery, tld="co.in", num=10, stop=50, pause=2):
     websites.append(j)
 
-print(websites)
-print(len(websites))
+randomWebsite=random.choice(websites)
+
+driver=webdriver.Chrome()
+
+driver.get(randomWebsite)
+
+waitBeforeClosing=input()
+while not waitBeforeClosing:
+    sleep(10)
