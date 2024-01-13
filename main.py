c425daa516292b412557a32ce342a32ce5dd5d6a
krisjanis-salenieks <144595869+krisjanis-salenieks@users.noreply.github.com>
2024-01-13 12:29:42 +0200

    izveidots izmantoto bibliotēku saraksts

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from googlesearch import search
 websites=[]
 searchQuery = input("Lūdzu ievadiet meklējamo frāzi: ")
 
-for j in search(searchQuery, tld="co.in", num=10, stop=100, pause=2):
+for j in search(searchQuery, tld="co.in", num=10, stop=50, pause=2):
     websites.append(j)
 
 print(websites)
